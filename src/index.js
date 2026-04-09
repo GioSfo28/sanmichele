66a74aceb0b5572c5fb6d7165f680cf889159d1f
Giorgio <giosfo28@gmail.com>
2026-04-09 11:58:38 +0200

    Modifiche

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
+
+// Importa la nuova pagina Galleria (dovrai creare questo file!)
+import Galleria from "./pages/Galleria"; 
+
 import "./index.css"; // Importa TailwindCSS
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        {/* La tua pagina principale (tutto quello che hai fatto finora) */}
+        <Route path="/" element={<App />} />
+        
+        {/* La nuova pagina dedicata alla Galleria */}
+        <Route path="/Galleria" element={<Galleria />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
---
+++ src/pages/Galleria.js
@@ -0,0 +1,14 @@
+// File: src/pages/Galleria.jsx
+import React from "react";
+
+const Galleria = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-4xl text-[#800020] font-bold">
+        Work in progress: La Galleria arriverà presto! 📸
+      </h1>
+    </div>
+  );
+};
+
+export default Galleria;
